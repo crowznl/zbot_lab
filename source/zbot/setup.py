4881c186e98245241acf17e9da0fb757c9dba3d4
glory <reallynotme.only@gmail.com>
2025-10-11 20:21:51 +0800

    upgrade

--- source/zbot/setup.py
+++ source/zbot/setup.py
@@ -33,8 +33,8 @@ setup(
     classifiers=[
         "Natural Language :: English",
         "Programming Language :: Python :: 3.10",
-        "Isaac Sim :: 2023.1.1",
-        "Isaac Sim :: 4.0.0",
+        "Isaac Sim :: 2025.10.11",
+        "Isaac Sim :: 4.5.0",
     ],
     zip_safe=False,
 )
--- source/zbot/zbot/tasks/zbot3_direct/__init__.py
+++ source/zbot/zbot/tasks/zbot3_direct/__init__.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 """
-Ant locomotion environment.
+zbot 3s environment.
 """
 
 import gymnasium as gym
